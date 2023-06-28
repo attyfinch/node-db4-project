6635592936109dd7c7f176997ec70f902c5df5f7
Brad Morris <bradmorris@Brads-Air.attlocal.net>
2023-06-27 21:11:04 -0700

    cleaned up the code for readability

--- api/recipes/recipe-model.js
+++ api/recipes/recipe-model.js
@@ -1,6 +1,5 @@
 const db = require('../../Data/db-config')
 
-
 async function getAllRecipes() {
     const recipes = await db('recipes')
     return recipes;
@@ -19,27 +18,11 @@ async function getStepsById(recipeId) {
     return steps
 }
 
-// async function getIngredientsByStepId(recipeId, stepId) {
-//     const getIngredients = await db('steps as s')
-//     .join('step_ingredient as si', 's.steps_id', '=', 'si.steps_id')
-//     .join('ingredients as i', 'i.ingredient_id', '=', 'si.ingredient_id')
-//     .where('s.recipe_id', recipeId)
-//     .where('si.steps_id', stepId)
-    
-//     return getIngredients
-// }
-
 async function getFullRecipeById(recipeId) {
     const [recipes] = await db('recipes')
     let steps = await db('steps')
         .where("recipe_id", recipeId)
         .select('steps_id', 'step_instruction', 'step_number')
-  
-    // let ingredients = await db('steps as s')
-    //     .join('step_ingredient as si', 's.steps_id', '=', 'si.steps_id')
-    //     .join('ingredients as i', 'i.ingredient_id', '=', 'si.ingredient_id')
-    //     .where('s.recipe_id', recipeId)
-    //     .where('s.steps_id')
     
     let stepsWithIngredients = [];
 
@@ -51,41 +34,18 @@ async function getFullRecipeById(recipeId) {
         .where('s.steps_id', i+1)
         .select('ingredient', 'quantity', 'si.ingredient_id')
         
-
-        console.log(steps[i])
-
         stepsWithIngredients.push({
             ...steps[i], 
             ingredients: ingredients})
-    }
+    };
 
-//    return stepsWithIngredients 
     return {
         recipe_id: recipeId,
         recipe_name: recipes.recipe_name,
         steps: stepsWithIngredients
-    }
-
-    /*
-    select 
-        i.ingredient_id,
-        i.ingredient,
-        i.quantity,
-        s.steps_id
-    from steps as s
-    join step_ingredient as si
-        on s.steps_id = si.steps_id
-    join ingredients as i
-        on si.ingredient_id = i.ingredient_id
-    where s.recipe_id = 2
-
-
-    */
+    };
 
 }
-
-
-
 
 module.exports = {
     getAllRecipes,
